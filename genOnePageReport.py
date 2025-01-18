e19696f15670351004a9bfcd8dec160562395e07
Mark Njega Kinyua <32300984+Merc123@users.noreply.github.com>
2025-01-18 21:00:49 +0300

    Update genOnePageReport.py

--- genOnePageReport.py
+++ genOnePageReport.py
@@ -17,34 +17,34 @@ def extract_text_from_analysis_file(file_path, tag):
 
 group = 'Analysis1'
 df_analysis = pd.read_csv(os.path.join(group + ".csv"))
-title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Title_Name"))
-logo_path = st.image(Image.open(os.path.join("logo.png")))
-summary_title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Summary_Title_Name"))
-summary_text = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Summary_Text"))
-kpi_title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "KPI_Title_Name"))
-kpi_data = st.write(df_analysis[["x1", "y1", "z1", "x2"]].tail(1))
-chart_title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Chart_Title_Name"))
-chart_path = st.image(Image.open(os.path.join(group + ".png")))
-chart_writeup = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Chart_Writeup"))
-datatable_tile_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "DataTable_Tile_Name"))
-data_table = st.write(df_analysis)
-footer_text = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Footer_Text"))
+# title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Title_Name"))
+# logo_path = st.image(Image.open(os.path.join("logo.png")))
+# summary_title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Summary_Title_Name"))
+# summary_text = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Summary_Text"))
+# kpi_title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "KPI_Title_Name"))
+# kpi_data = st.write(df_analysis[["x1", "y1", "z1", "x2"]].tail(1))
+# chart_title_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Chart_Title_Name"))
+# chart_path = st.image(Image.open(os.path.join(group + ".png")))
+# chart_writeup = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Chart_Writeup"))
+# datatable_tile_name = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "DataTable_Tile_Name"))
+# data_table = st.write(df_analysis)
+# footer_text = st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Footer_Text"))
 
 # Render the template with the report content
 template = Template(os.path.join(group + ".html"))
 context = {
-  'title_name' : title_name,
-  'logo_path' : logo_path,
-  'summary_title_name' : summary_title_name,
-  'summary_text' : summary_text,      
-  'kpi_title_name' : kpi_title_name,
-  'kpi_data' : kpi_data,
-  'chart_title_name' : chart_title_name,
-  'chart_path' : chart_path,
-  'chart_writeup': chart_writeup,      
-  'datatable_tile_name' : datatable_tile_name,
-  'data_table' : data_table,
-  'footer_text' : footer_text
+  'title_name' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Title_Name")),
+  'logo_path' : st.image(Image.open(os.path.join("logo.png"))),
+  'summary_title_name' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Summary_Title_Name")),
+  'summary_text' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Summary_Text")),      
+  'kpi_title_name' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "KPI_Title_Name")),
+  'kpi_data' : st.write(df_analysis[["x1", "y1", "z1", "x2"]].tail(1)),
+  'chart_title_name' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Chart_Title_Name")),
+  'chart_path' : st.image(Image.open(os.path.join(group + ".png"))),
+  'chart_writeup': st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Chart_Writeup")),      
+  'datatable_tile_name' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "DataTable_Tile_Name")),
+  'data_table' : st.write(df_analysis),
+  'footer_text' : st.write(extract_text_from_analysis_file(os.path.join(group + ".txt"), "Footer_Text"))
 }
 rendered_html = template.render(context)
 
